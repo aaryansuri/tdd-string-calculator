84836eb3aac7d49a14d7e573b66c78840306447d
aaryansuri <aaryansuri0@gmail.com>
2021-04-18 22:17:45 +0530

    passed for two number string and other base cases

--- src/com/assessment/StringCalculator.java
+++ src/com/assessment/StringCalculator.java
@@ -1,13 +1,22 @@
 package com.assessment;
 
 
+
 public class StringCalculator {
 
     public int Add(String numbers) {
 
         if(numbers.isEmpty() || numbers.isBlank()) return 0;
 
-        return Integer.parseInt(numbers);
+        String[] numSplits = numbers.split(",");
+
+        if(numSplits.length==1) return Integer.parseInt(numSplits[0]);
+
+        int num1 = Integer.parseInt(numSplits[0]);
+        int num2 = Integer.parseInt(numSplits[1]);
+
+        return num1 + num2;
+
 
     }
 
