69bbb02cbe2811dca450a1634fd14a882859f802
aaryansuri <aaryansuri0@gmail.com>
2021-04-18 22:32:05 +0530

    Refactored for new lines

--- src/com/assessment/StringCalculator.java
+++ src/com/assessment/StringCalculator.java
@@ -8,7 +8,8 @@ public class StringCalculator {
 
         if(numbers.isEmpty() || numbers.isBlank()) return 0;
 
-        String[] numSplits = numbers.split(",");
+        String[] numSplits = numbers.split("[,\n]\\s*"); //regex for splitting string by comma or
+                                                               //new line, and blank spaces if specified in input
 
 
         int sum = 0;
